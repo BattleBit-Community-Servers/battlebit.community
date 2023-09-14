d218e9bfd9c7bb083f7c75cc3e21976db98e2c6a
Julian Ellis <admin@battlebit.community>
2023-09-14 03:17:10 -0400

    Update fetchAndDisplayContributors.js

--- resources/js/fetchAndDisplayContributors.js
+++ resources/js/fetchAndDisplayContributors.js
@@ -1,18 +1,15 @@
 // Wrap your code in an event listener for the "DOMContentLoaded" event
-document.addEventListener("DOMContentLoaded", () => {
-  const contributorsContainer = document.getElementById(
-    "contributors-container"
-  );
+document.addEventListener('DOMContentLoaded', () => {
+  const contributorsContainer = document.getElementById('contributors-container');
 
   // Replace 'BattleBit-Community-Servers' with the organization name
-  const orgName = "BattleBit-Community-Servers";
+  const orgName = 'BattleBit-Community-Servers';
 
   // Array of repository names
-  const repoNames = [
-    "BattleBitAPIRunner",
-    "BBModules-Backend",
-    "BBModules-Frontend",
-  ];
+  const repoNames = ['BattleBitAPIRunner', 'BBModules-Backend', 'BBModules-Frontend'];
+
+  // Users to exclude from contributors
+  const excludedUsers = ['JellisyWoes', 'RainOrigami'];
 
   // Fetch and combine contributors from multiple repositories
   const fetchContributors = async (orgName, repoNames) => {
@@ -27,11 +24,12 @@ document.addEventListener("DOMContentLoaded", () => {
         const response = await fetch(githubApiUrl);
         const data = await response.json();
 
-        // Filter out GitHub Actions contributors and duplicates
-        const filteredContributors = data.filter((contributor) => {
+        // Filter out GitHub Actions contributors, duplicates, and excluded users
+        const filteredContributors = data.filter(contributor => {
           return (
-            !contributor.login.endsWith("[bot]") &&
-            !allContributors.some((c) => c.login === contributor.login)
+            !contributor.login.endsWith('[bot]') &&
+            !allContributors.some(c => c.login === contributor.login) &&
+            !excludedUsers.includes(contributor.login)
           );
         });
 
@@ -40,21 +38,21 @@ document.addEventListener("DOMContentLoaded", () => {
       }
 
       // Process the combined contributor data and build contributor elements
-      const contributorElements = allContributors.map((contributor) => {
+      const contributorElements = allContributors.map(contributor => {
         return `
-            <div class="contributor-box">
-              <a href="${contributor.html_url}" target="_blank">
-                <img src="${contributor.avatar_url}" alt="${contributor.login}">
-              </a>
-            </div>
-          `;
+          <div class="contributor-box">
+            <a href="${contributor.html_url}" target="_blank">
+              <img src="${contributor.avatar_url}" alt="${contributor.login}">
+            </a>
+          </div>
+        `;
       });
 
       // Insert the contributor elements into the container
-      contributorsContainer.innerHTML = contributorElements.join("");
+      contributorsContainer.innerHTML = contributorElements.join('');
     } catch (error) {
-      console.error("Error fetching contributor data:", error);
-      contributorsContainer.innerHTML = "Error fetching contributor data.";
+      console.error('Error fetching contributor data:', error);
+      contributorsContainer.innerHTML = 'Error fetching contributor data.';
     }
   };
 
